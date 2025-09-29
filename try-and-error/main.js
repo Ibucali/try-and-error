09f596cc23ffee271b1871178314ca715914d304
viscatommy <tommy21heel@gmail.com>
2025-09-29 21:56:12 +0900

    firebase

---
+++ try-and-error/main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsR4D9tTTyqcuT0_Ho6WtsH1x4ZG6yVAQ",
+  authDomain: "try-and-error-b7dae.firebaseapp.com",
+  projectId: "try-and-error-b7dae",
+  storageBucket: "try-and-error-b7dae.firebasestorage.app",
+  messagingSenderId: "836968829",
+  appId: "1:836968829:web:e19ce3d506b65f9adc6c14"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
